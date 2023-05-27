d352278a51d1188ef5828fe09e0b60be09c0a3a2
suraj.be <suraj.be@media.net>
2023-05-27 11:46:49 +0530

    Working with a sample products html

--- main.js
+++ main.js
@@ -1,24 +1,28 @@
+/* styles */
+import './reset.css'
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import './product.css'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+/* scripts */
+const products = document.querySelectorAll('[data-stock]')
 
-setupCounter(document.querySelector('#counter'))
+products.forEach((product) => {
+  const stockStatus = product.dataset.stock
+
+  switch (stockStatus) {
+    case 'in-stock':
+      product.querySelector('p').textContent = 'Products are in stock, make it yours!'
+      break
+
+    case 'limited-stock':
+      product.querySelector('p').textContent = 'Limited Stock, PLS HURRY!!!'
+      break
+
+    case 'out-of-stock':
+      product.querySelector('p').textContent = 'Out of Stock, Test your luck next time!!!'
+      break
+
+    default:
+      break
+  }
+})
